0c44cc94e52c96629ababc69c96ab3060eb69407
Guilherme Henrique Lemes de Godoy <guilherme.hlg.eng@gmail.com>
2022-09-21 08:08:29 -0300

    inicia curso sobre java.util

--- src/main/java/teste/Teste.java
+++ src/main/java/teste/Teste.java
@@ -1,7 +1,23 @@
 package teste;
 
+import modelo.ContaCorrente;
+
 public class Teste {
     public static void main(String[] args) {
-        System.out.println();
+        int[] idades = new int[5];
+
+        for (int i = 0; i < idades.length; i++) {
+            idades[i] = i*i;
+        }
+
+        for (int i = 0; i < idades.length; i++) {
+            System.out.println(idades[i]);
+        }
+
+        ContaCorrente[] contas = new ContaCorrente[5];
+
+        ContaCorrente cc1 = new ContaCorrente(111, 111);
+
+        contas[0] = cc1;
     }
 }
